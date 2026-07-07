1e7281095ceea7e399d4edba46010a6a46fc98bf
dmytropshenichnikov <pshenichnikov.dimka@gmail.com>
2026-07-07 13:13:40 +0300

    Move Google Ads tag to head (beforeInteractive)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,9 +38,9 @@ export default function RootLayout({
         {children}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=AW-18199730227"
-          strategy="afterInteractive"
+          strategy="beforeInteractive"
         />
-        <Script id="google-analytics" strategy="afterInteractive">
+        <Script id="google-analytics" strategy="beforeInteractive">
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
